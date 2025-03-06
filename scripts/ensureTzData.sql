927d05d67dec3f8d0094a63514d3b8f3f09751ca
root <root@k3y10>
2025-03-05 21:58:02 -0700

    attempting fix with migration | confirmed quotes

--- scripts/ensureTzData.sql
+++ scripts/ensureTzData.sql
@@ -1,8 +1,9 @@
+-- scripts/ensureTzData.sql
 SET search_path TO crm;
 CREATE TABLE IF NOT EXISTS tz_data (
   id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  provinceName TEXT NOT NULL,
-  countryCode VARCHAR(2) NOT NULL,
-  tz TEXT NOT NULL
+  "name" TEXT NOT NULL,
+  "provinceName" TEXT NOT NULL,
+  "countryCode" VARCHAR(2) NOT NULL,
+  "tz" TEXT NOT NULL
 );
