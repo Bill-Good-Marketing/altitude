084ab2275e0eb4a69a5bd3801205d4e3a35af554
root <root@DEV-keatonm.billgood.local>
2025-03-06 10:26:38 -0700

    trying to resolve jest test issue

--- src/tests/jest.setup.ts
+++ src/tests/jest.setup.ts
@@ -1,26 +1,12 @@
-import {test} from "@jest/globals";
-import {PerformInTransaction, RollbackTransaction} from "~/db/sql/transaction";
-import './utils' // extend expect
+import { test } from '@jest/globals';
+import { PerformInTransaction, RollbackTransaction } from '~/db/sql/transaction';
 
-// @ts-expect-error - We're redefining the test function
-global.test = (name: string, fn: () => Promise<void>, timeout: number) => {
-    test(name, async () => {
-        await PerformInTransaction(async () => {
-            await fn()
-            RollbackTransaction()
-        })
-    }, timeout)
-}
-
-declare global {
-    // eslint-disable-next-line @typescript-eslint/no-namespace
-    namespace jest {
-        interface It {
-            (title: string, fn: (client: any) => Promise<void>, timeout?: number): void;
-        }
-
-        interface Test {
-            (title: string, fn: (client: any) => Promise<void>, timeout?: number): void;
-        }
-    }
-}
+// @ts-expect-error - Overriding the global test
+global.test = (name: string, fn: () => Promise<void>, timeout?: number) => {
+  return test(name, async () => {
+    await PerformInTransaction(async () => {
+      await fn();
+      RollbackTransaction();
+    });
+  }, timeout);
+};
